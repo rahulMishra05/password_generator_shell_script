e911da8aa69e72364a9e8dcbdcd2bbf73f1763e6
Rahul Mishra <rahul@kali>
2021-01-31 16:07:12 +0530

    Output improved by adding the space in between the passwords and message

--- passwordGenerator.sh
+++ passwordGenerator.sh
@@ -9,6 +9,7 @@ echo "THIS IS A SIMPLE PASSWORD GENERATOR"
 # Here \c is used to keep the cursor on the same line and if we do not use -e flag than \c will also get printed 
 echo -e "Please enter the length of the password you want : \c"
 read PASS_LENGTH  # Store the password length entered by the user here
+echo " "
 
 for p in $(seq 1 5);
 do
